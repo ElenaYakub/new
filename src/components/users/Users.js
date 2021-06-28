4951e493a72c6daab99508204facf0c140c4bee7
ElenaYakub <lightcharcoal7@gmail.com>
2021-06-28 16:25:52 +0300

    add useEffect

---
+++ src/components/users/Users.js
@@ -0,0 +1,20 @@
+export default function Users(){
+
+
+
+useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => {
+            console.log(value);
+        });
+},[]);
+
+
+return(
+<div>
+    usersList.map(value => <div>user</div>)
+</div>
+
+);
+}
