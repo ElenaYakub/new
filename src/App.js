30da68434baead67b63b429c82976fbf580d3b14
ElenaYakub <lightcharcoal7@gmail.com>
2021-07-04 19:53:30 +0300

    users coments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import Users from "./components/users/Users";
-import Posts from "./components/posts/Posts";
-import Comments from "./components/comments/Comments";
+
 
 
 export default function App(){
@@ -12,13 +11,11 @@ return(
 <div>
     <div><Link to="/">home page</Link></div>
     <div><Link to="/users">users page</Link></div>
-    <div><Link to="/posts">posts page</Link></div>
-    <div><Link to="/comments">comments page</Link></div>
+
 <Switch>
     <Route path={'/'} exact render={() => {return <div>home</div>;}}/>
     <Route path={'/users'} component={Users}/>
-    <Route path={'/posts'} component={Posts}/>
-    <Route path={'/comments'} component={Comments}/>
+
 </Switch>
 
 </div>
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,8 +1,10 @@
 import {getUserPosts} from '../../services/API';
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
 export default function User({item}){
 
-    let[posts, setPosts] = useState([]);
+    // let[posts, setPosts] = useState([]);
     return(
         <div>
             {item.name} - <Link to={'/users/' + item.id}>details</Link>
